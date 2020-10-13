cb09399a9de60c0624a331e7ae2a6ab39f6d2e40
NathanBilleau <nbilleau@gmail.com>
2020-10-13 18:42:03 +0200

    function mail

---
+++ functions/white-rabbit.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer')
+
+let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.MAIL,
+      pass: process.env.PASS,
+    },
+  })
+  
+  exports.handler = function (event, context, callback) {
+  
+  
+    transporter.sendMail(
+      {
+        from: `"Nathan" ${process.env.MAIL}`,
+        to: 'nbilleau@gmail.com',
+        subject: "🐇",
+        html: `m3ukpba6hlr7ebiyqavimbrjl3mpzzfvbg64ibo6gjktkvrur6ngejyd`,
+      },
+      (error, info) => {
+        if (error) {
+          console.log(error)
+          callback(error)
+        } else {
+          console.log(info)
+  
+          callback(null, {
+            statusCode: 200,
+            body: "Regarde tes mails.",
+          })
+        }
+      }
+    )
+  }
